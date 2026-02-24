f778069c8bae32702704449f34708592802ae45b
Zhaohui Ng <zh007227@gmail.com>
2026-02-24 23:19:28 +0800

    custom dataset class for telemetry data

---
+++ data/dataset/telemetry_dataset.py
@@ -0,0 +1,12 @@
+from torch.utils.data import Dataset
+import numpy as np
+
+class TelemetryDataset(Dataset):
+    def __init__(self, data: np.ndarray):
+        self.data = data
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
